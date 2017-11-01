d57340d857710b0fb5f8126d795eaeee23892376
Jiabin Hu <jiabinhu@jiabinhu-macbookpro2.roam.corp.google.com>
2017-11-01 16:12:30 -0700

    Fix format of Python files

--- python/ds/trie_test.py
+++ python/ds/trie_test.py
@@ -4,43 +4,43 @@ import trie
 
 class TrieTest(unittest.TestCase):
 
-    def testEmptyTrie(self):
-        t = trie.Trie()
-        self.assertEqual(t.GetWordsWithPrefix(''), [])
-        self.assertEqual(t.GetWordsWithPrefix('foo'), [])
+  def testEmptyTrie(self):
+    t = trie.Trie()
+    self.assertEqual(t.GetWordsWithPrefix(''), [])
+    self.assertEqual(t.GetWordsWithPrefix('foo'), [])
 
-    def testOneWords(self):
-        t = trie.Trie()
-        t.Insert('cross')
-        self.assertEqual(t.GetWordsWithPrefix('cr'), ['cross'])
-        self.assertEqual(t.GetWordsWithPrefix(''), ['cross'])
-        self.assertEqual(t.GetWordsWithPrefix('cross'), ['cross'])
-    
-    def testManyWords(self):
-        t = trie.Trie()
-        t.Insert('cross')
-        t.Insert('crash')
-        t.Insert('apple')
-        t.Insert('cause')
+  def testOneWords(self):
+    t = trie.Trie()
+    t.Insert('cross')
+    self.assertEqual(t.GetWordsWithPrefix('cr'), ['cross'])
+    self.assertEqual(t.GetWordsWithPrefix(''), ['cross'])
+    self.assertEqual(t.GetWordsWithPrefix('cross'), ['cross'])
 
-        self.assertEqual(sorted(t.GetWordsWithPrefix('cross')), sorted(['cross']))
-        self.assertEqual(sorted(t.GetWordsWithPrefix('cr')), sorted(['cross', 'crash']))
-        self.assertEqual(sorted(t.GetWordsWithPrefix('c')), sorted(['cross', 'crash', 'cause']))
-        self.assertEqual(sorted(t.GetWordsWithPrefix('')), sorted(['cross', 'crash', 'cause', 'apple']))
+  def testManyWords(self):
+    t = trie.Trie()
+    t.Insert('cross')
+    t.Insert('crash')
+    t.Insert('apple')
+    t.Insert('cause')
 
-    def testRemove(self):
-        t = trie.Trie()
-        t.Insert('cross')
-        t.Insert('crash')
-        t.Insert('apple')
+    self.assertEqual(sorted(t.GetWordsWithPrefix('cross')), sorted(['cross']))
+    self.assertEqual(sorted(t.GetWordsWithPrefix('cr')), sorted(['cross', 'crash']))
+    self.assertEqual(sorted(t.GetWordsWithPrefix('c')), sorted(['cross', 'crash', 'cause']))
+    self.assertEqual(sorted(t.GetWordsWithPrefix('')), sorted(['cross', 'crash', 'cause', 'apple']))
 
-        with self.assertRaises(Exception):
-            t.Remove('cruel')
-        t.Remove('cross')
-        self.assertEqual(sorted(t.GetWordsWithPrefix('')), ['apple', 'crash'])
-        t.Remove('crash')
-        self.assertEqual(t.GetWordsWithPrefix(''), ['apple'])
+  def testRemove(self):
+    t = trie.Trie()
+    t.Insert('cross')
+    t.Insert('crash')
+    t.Insert('apple')
+
+    with self.assertRaises(Exception):
+      t.Remove('cruel')
+    t.Remove('cross')
+    self.assertEqual(sorted(t.GetWordsWithPrefix('')), ['apple', 'crash'])
+    t.Remove('crash')
+    self.assertEqual(t.GetWordsWithPrefix(''), ['apple'])
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
