4b1e25e5df337fd9d94c9a87be393ceed9009f36
Jiabin Hu <jiabinhu@google.com>
2020-06-07 21:54:06 -0700

    Implement BST.delete()

--- python/ds/bst.py
+++ python/ds/bst.py
@@ -30,16 +30,55 @@ class BST(object):
           cur = cur._right
 
   def search(self, val):
+    return self._search(val)[1] is not None
+
+  def _search(self, val):
+    parent = None
     cur = self._root
     while True:
       if not cur:
-        return False
+        return None, None
       elif cur._val == val:
-        return True
+        return parent, cur
       elif cur._val < val:
+        parent = cur
         cur = cur._right
       else:
+        parent = cur
         cur = cur._left
+
+  def delete(self, val):
+    parent, target = self._search(val)
+    if not target:
+      raise Exception("Element not in BST: " + val)
+    if self._isLeaf(target):
+        self._relink(parent, target, None)
+    elif not target._left:
+      self._relink(parent, target, target._right)
+    elif not target._right:
+      self._relink(parent, target, target._left)
+    elif not target._left._right:
+      target._left._right = target._right
+      self._relink(parent, target, target._left)
+    else:
+      parent = target._left
+      while parent._right._right:
+        parent = parent._right
+      target._val = parent._right._val  
+      parent._right = None
+
+  def _relink(self, parent, target, new_target):
+    if not parent:
+      self._root = new_target
+    elif target == parent._left:
+      parent._left = new_target
+    elif target == parent._right:
+      parent._right = new_target
+    else:
+      raise Exception("Cannot unlink when target is not a child of parent.")
+
+  def _isLeaf(self, node):
+    return not node._left and not node._right
 
   def __str__(self):
     return self._print(self._root)
--- python/ds/bst_test.py
+++ python/ds/bst_test.py
@@ -16,6 +16,10 @@ class BSTTest(unittest.TestCase):
     self.assertEqual(t.search(0), False)
     self.assertEqual(str(t), '[] 3 []')
 
+    t.delete(3)
+    self.assertEqual(t.search(3), False)
+    self.assertEqual(str(t), '')
+
   def testTwoElementBST(self):
     t = bst.BST()
     t.insert(3)
@@ -24,6 +28,10 @@ class BSTTest(unittest.TestCase):
     self.assertEqual(t.search(5), True)
     self.assertEqual(t.search(0), False)
     self.assertEqual(str(t), '[] 3 [[] 5 []]')
+
+    t.delete(3)
+    self.assertEqual(t.search(3), False)
+    self.assertEqual(str(t), '[] 5 []')
 
   def testThreeElementBST(self):
     t = bst.BST()
@@ -35,6 +43,9 @@ class BSTTest(unittest.TestCase):
     self.assertEqual(t.search(1), True)
     self.assertEqual(t.search(0), False)
     self.assertEqual(str(t), '[[] 1 []] 3 [[] 5 []]')
+
+    t.delete(3)
+    self.assertEqual(str(t), '[] 1 [[] 5 []]')
 
   def testFourElementBST(self):
     t = bst.BST()
@@ -48,6 +59,48 @@ class BSTTest(unittest.TestCase):
     self.assertEqual(t.search(2), True)
     self.assertEqual(t.search(0), False)
     self.assertEqual(str(t), '[[] 1 [[] 2 []]] 3 [[] 5 []]')
+
+    t.delete(3)
+    self.assertEqual(str(t), '[[] 1 []] 2 [[] 5 []]')
+
+  def testRightPath(self):
+    t = bst.BST()
+    t.insert(5)
+    t.insert(6)
+    t.insert(7)
+    self.assertEqual(str(t), '[] 5 [[] 6 [[] 7 []]]')
+
+    t.delete(6)
+    self.assertEqual(str(t), '[] 5 [[] 7 []]')
+
+  def testDeleteNonexistent(self):
+    t = bst.BST()
+    t.insert(3)
+    self.assertRaises(Exception, lambda: t.delete(4))
+
+  def testComplete(self):
+    t = bst.BST()
+    t.insert(5)
+    t.insert(2)
+    t.insert(7)
+    t.insert(1)
+    t.insert(3)
+    t.insert(6)
+    t.insert(8)
+    t.insert(4)
+    self.assertEqual(str(t), '[[[] 1 []] 2 [[] 3 [[] 4 []]]] 5 [[[] 6 []] 7 [[] 8 []]]')
+
+    t.delete(5)
+    self.assertEqual(str(t), '[[[] 1 []] 2 [[] 3 []]] 4 [[[] 6 []] 7 [[] 8 []]]')
+
+    t.delete(8)
+    self.assertEqual(str(t), '[[[] 1 []] 2 [[] 3 []]] 4 [[[] 6 []] 7 []]')
+
+    t.delete(7)
+    self.assertEqual(str(t), '[[[] 1 []] 2 [[] 3 []]] 4 [[] 6 []]')
+
+    t.delete(1)
+    self.assertEqual(str(t), '[[] 2 [[] 3 []]] 4 [[] 6 []]')
 
 
 if __name__ == '__main__':
